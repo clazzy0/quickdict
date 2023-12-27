4191b5e190d81e4e31f3425441ca83018539a44a
clazzy <jianingchen16@gmail.com>
2023-12-26 23:37:02 -0500

    middleware cookies

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,10 @@ import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
 export function middleware(request: NextRequest) {
-  const session = request.cookies.get("next-auth.session-token")
+  const cookiesOne = request.cookies.get("next-auth.session-token")
+  const cookiesTwo = request.cookies.get("next-auth.crsf-token")
 
-  if (session) {
+  if (cookiesOne || cookiesTwo) {
     return NextResponse.next()
   } else return NextResponse.redirect(new URL("/", request.url))
 }
